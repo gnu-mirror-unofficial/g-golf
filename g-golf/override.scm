d670abdda802b934ddcc2ca384605f0dd3abc65e
David Pirotte <david@altosw.be>
2020-04-14 01:15:36 -0300

    Preparing for an imported function/method override mechanism
    * g-golf/override.scm (%gi-override, gi-override?): New module. New
      %gi-override variable, that holds the list of functions and methods
      that needs to be overriden. New procedure gi-override?, that checks if
      a GI'ed function or method (given its GI name) should be overriden.
    
    * g-golf/override/gtk.scm (gtk-list-store-set-value-ov,
      gtk-tree-model-get-value-ov): New module, that contains and export a
      definition for the functions and methods that must be overriden, from
      the "Gtk" namespace.
    
      All definitions are named after their original name, to which the
      "-ov" postfix is added.
    
      All definitions receive one argument, which is the procedure as
      imported from the typelib.
    
      All definitions return to values: (1) a list of prerequisite(s) or
      #f; (2) a quase quoted lambda expression containing the code of the
      procedure that overrides the imported typelib version, within which
      the procedure argument that is passed (the typelib definition) is
      evaluated.
    
      Each prerequisite is a list of (namespace name) pairs.
    
      The reason those definitions return quoted lambdas is (a) to avoid
      to (have to) import their prerequiste(s) when that would not be
      necessary and (b) to avoid meta circular modules definition - this
      module will be imported by (g-golf hl-api function), one of the
      modules it would depend on if not returning quoted lambdas ...
    
    * g-golf/override/override.scm: New module, that import and reexport all
      modules in g-golf/override.
    
    * Makefile.am:
    * g-golf.scm: Updated for the above.

---
+++ g-golf/override.scm
@@ -0,0 +1,37 @@
+;; -*- mode: scheme; coding: utf-8 -*-
+
+;;;;
+;;;; Copyright (C) 2020
+;;;; Free Software Foundation, Inc.
+
+;;;; This file is part of GNU G-Golf
+
+;;;; GNU G-Golf is free software; you can redistribute it and/or modify
+;;;; it under the terms of the GNU Lesser General Public License as
+;;;; published by the Free Software Foundation; either version 3 of the
+;;;; License, or (at your option) any later version.
+
+;;;; GNU G-Golf is distributed in the hope that it will be useful, but
+;;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;;; Lesser General Public License for more details.
+
+;;;; You should have received a copy of the GNU Lesser General Public
+;;;; License along with GNU G-Golf.  If not, see
+;;;; <https://www.gnu.org/licenses/lgpl.html>.
+;;;;
+
+;;; Commentary:
+
+;;; Code:
+
+
+(define-module (g-golf override)
+  #:use-module (g-golf support modules)
+  #:use-module (g-golf override override)
+  #:use-module (g-golf override gtk))
+
+
+(eval-when (expand load eval)
+  (re-export-public-interface (g-golf override override)
+                              (g-golf override gtk)))
---
+++ g-golf/override/gtk.scm
@@ -0,0 +1,56 @@
+;; -*- mode: scheme; coding: utf-8 -*-
+
+;;;;
+;;;; Copyright (C) 2020
+;;;; Free Software Foundation, Inc.
+
+;;;; This file is part of GNU G-Golf
+
+;;;; GNU G-Golf is free software; you can redistribute it and/or modify
+;;;; it under the terms of the GNU Lesser General Public License as
+;;;; published by the Free Software Foundation; either version 3 of the
+;;;; License, or (at your option) any later version.
+
+;;;; GNU G-Golf is distributed in the hope that it will be useful, but
+;;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;;; Lesser General Public License for more details.
+
+;;;; You should have received a copy of the GNU Lesser General Public
+;;;; License along with GNU G-Golf.  If not, see
+;;;; <https://www.gnu.org/licenses/lgpl.html>.
+;;;;
+
+;;; Commentary:
+
+;;; Code:
+
+
+(define-module (g-golf override gtk)
+  #:export (gtk-list-store-set-value-ov
+            gtk-tree-model-get-value-ov))
+
+
+(define (gtk-list-store-set-value-ov proc)
+  (values
+   '(("Gtk" "TreeModel"))
+   `(lambda (store iter column value)
+      (let* ((i-func ,proc)
+             (g-type (gtk-tree-model-get-column-type store column))
+             (g-value (g-value-init (symbol->g-type g-type))))
+        (g-value-set! g-value value)
+        (i-func store iter column g-value)
+        (g-value-unset g-value)
+        (values)))))
+
+(define (gtk-tree-model-get-value-ov proc)
+  (values
+   #f
+   `(lambda (model iter column)
+      (let* ((i-func ,proc)
+             (g-type (gtk-tree-model-get-column-type model column))
+             (g-value (g-value-init (symbol->g-type g-type)))
+             (dum (i-func model iter column g-value))
+             (value (g-value-ref g-value)))
+        (g-value-unset g-value)
+        value))))
